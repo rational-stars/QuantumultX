1ce95efb94dffe3dcdeb6875bd4a000426861f23
🇨🇳 向前 <rational-stars@deMac-mini.local>
2024-07-31 14:52:05 +0800

    万仕利餐饮集团-食堂 VIP

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # QuantumultX
 qx 脚本
+## Star History
+
+[![Star History Chart](https://api.star-history.com/svg?repos=rational-stars/QuantumultX&type=Date)](https://star-history.com/#rational-stars/QuantumultX&Date)
---
+++ wanshili/dining.js
@@ -0,0 +1,31 @@
+/**************************************
+
+@Name：万仕利餐饮集团-智慧食堂 VIP
+@Author：rational_stars
+@Date：2024 年 7 月 31 日 14:46:13
+
+脚本兼容：只测试过 QuantumultX 没有其他 app 无法测试望理解
+
+
+使用方法：
+直接添加到重新就行
+
+QunatumultX
+
+[rewrite_local]
+^https:\/\/h5-v4\.packertec\.com\/api\/booking\/member_user\/get_member_info url script-response-body https://raw.githubusercontent.com/rational-stars/QuantumultX/main/wanshili/dining.js
+[mitm]
+hostname = h5-v4.packertec.com
+====================================
+⚠️【免责声明】
+------------------------------------------
+1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
+2、由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
+3、请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
+4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
+5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
+6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
+7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
+******************************************/
+
+var _0x595704=_0x36c1;(function(_0x248702,_0xf467dc){var _0x386dbc=_0x36c1,_0x1a2a40=_0x248702();while(!![]){try{var _0x52df1a=-parseInt(_0x386dbc(0xe1))/0x1+-parseInt(_0x386dbc(0xe0))/0x2*(-parseInt(_0x386dbc(0xdd))/0x3)+-parseInt(_0x386dbc(0xeb))/0x4+-parseInt(_0x386dbc(0xdc))/0x5+parseInt(_0x386dbc(0xe6))/0x6*(parseInt(_0x386dbc(0xe9))/0x7)+parseInt(_0x386dbc(0xe4))/0x8+parseInt(_0x386dbc(0xe8))/0x9;if(_0x52df1a===_0xf467dc)break;else _0x1a2a40['push'](_0x1a2a40['shift']());}catch(_0x14dd93){_0x1a2a40['push'](_0x1a2a40['shift']());}}}(_0x1dec,0xdd4e6));var aFengYe=$response[_0x595704(0xe7)],obj=JSON[_0x595704(0xde)](aFengYe);function _0x1dec(){var _0x16e59a=['data','member_grade','permanent_status','2256505tnxQFq','3rPoqyX','parse','stringify','2009882ZJjSno','1002254HKZRqu','2099-12-31\x2023:59:59','is_member_status','7948192LxFGPL','end_time','584394SvSSmA','body','15568722XWWcGd','14JlHFBd','log','6252384UwNRoY'];_0x1dec=function(){return _0x16e59a;};return _0x1dec();}function _0x36c1(_0xe1b700,_0x4396f5){var _0x1decfd=_0x1dec();return _0x36c1=function(_0x36c16b,_0x1521ce){_0x36c16b=_0x36c16b-0xda;var _0x1e1e2c=_0x1decfd[_0x36c16b];return _0x1e1e2c;},_0x36c1(_0xe1b700,_0x4396f5);}obj[_0x595704(0xec)][_0x595704(0xe5)]=_0x595704(0xe2),obj['data'][_0x595704(0xdb)]=!![],obj[_0x595704(0xec)][_0x595704(0xe3)]=!![],obj['data'][_0x595704(0xda)]=0x2,aFengYe=JSON[_0x595704(0xdf)](obj),console[_0x595704(0xea)](aFengYe),$done(aFengYe);
